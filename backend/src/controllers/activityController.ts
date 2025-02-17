45b68fcfc1d4f64716565cd5fed3145cf8027de6
Sahil Shah <sahilshah2104@gmail.com>
2025-02-17 22:04:13 +0530

    Fixed some backend code imported dataset, setup DB schema

--- backend/src/controllers/activityController.ts
+++ backend/src/controllers/activityController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
 function getActivities(req: Request, res: Response) {
-    // TODO: write the logic to add predefined activities over here
+    // TODO: write the logic to add predefined activities over here (this show the activities in the dropdown)
 
     return res.status(StatusCodes.OK).json({
         status: true,
@@ -13,7 +13,7 @@ function getActivities(req: Request, res: Response) {
 }
 
 function logNewActivity(req: Request, res: Response) {
-    // TODO: write the logic to feed the user's activity log
+    // TODO: write the logic to feed the user's activity log (this is for search bar)
 
     return res.status(StatusCodes.ACCEPTED).json({
         status: true,
@@ -24,7 +24,7 @@ function logNewActivity(req: Request, res: Response) {
 }
 
 function getUserActivityLogs(req: Request, res: Response) {
-    // TODO: write the logic to return user's day activity logs and total carbon emmission done
+    // TODO: write the logic to return user's day activity logs and total carbon emmission done (this is for the log box)
 
     return res.status(StatusCodes.ACCEPTED).json({
         status: true,
